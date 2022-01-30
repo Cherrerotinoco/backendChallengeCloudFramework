5640fb2644d802ec2f6dfab9b5116c75769eaf32
Cherrerotinoco <c.herrerotinoco@gmail.com>
2022-01-30 21:07:30 +0100

    Set local mongoDB

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    mongodburl: `mongodb+srv://mongo_user_db:${process.env.MONGO_DB_PASS}@cluster0.chybs.mongodb.net/cloudFramework?retryWrites=true&w=majority`
+    mongodburl: 'mongodb://localhost:27017'
   }
 }
